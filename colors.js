10a60bc00576adb1bd49a70e3eb412842fa902e5
AnKiWoong <ankiwoong@gmail.com>
2021-01-20 21:10:50 +0900

    라이브러리와 프레임워크

--- colors.js
+++ colors.js
@@ -1,21 +1,24 @@
 var Links = {
     setColor: function (color) {
-        var alist = document.querySelectorAll("a");
-        var i = 0;
+        // var alist = document.querySelectorAll("a");
+        // var i = 0;
 
-        while (i < alist.length) {
-            alist[i].style.color = color;
-            i = i + 1;
-        }
+        // while (i < alist.length) {
+        //     alist[i].style.color = color;
+        //     i = i + 1;
+        // }
+        $("a").css("color", color);
     },
 };
 
 var Body = {
     setColor: function (color) {
-        document.querySelector("body").style.color = color;
+        // document.querySelector("body").style.color = color;
+        $("body").css("color", color);
     },
     setBackgroundColor: function (color) {
-        document.querySelector("body").style.background = color;
+        // document.querySelector("body").style.background = color;
+        $("body").css("backgroundColor", color);
     },
 };
 
